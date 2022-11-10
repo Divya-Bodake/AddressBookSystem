cac3fd274a1a8edfeeb4f222a1af7af36a4ff298
Divya-Bodake <divyabodake25@gmail.com>
2022-11-10 17:22:56 +0530

    [Divya]Add:Edit Contact

--- AddressBook/Address.cs
+++ AddressBook/Address.cs
@@ -4,16 +4,25 @@ using System.Linq;
 using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace AddressBook
 {
     internal class Address
     {
+        
+        
         List<Contact> address = new List<Contact>();
+        public void Display()
+        {
+            foreach (Contact contact in address)
+            {
+                Console.WriteLine(contact.FirstName + "\n" + contact.LastName + "\n" + contact.Address + "\n" + contact.City + "\n" + contact.State + "\n" + contact.zip + "\n" + contact.PhoneNumber + "\n" + contact.Email);
+            }
+        }
         public void AddContact()
         {
             Contact contact = new Contact();
-            address.Add(contact);
 
             Console.WriteLine("Enter FirstName");
             contact.FirstName = Console.ReadLine();
@@ -32,14 +41,68 @@ namespace AddressBook
             Console.WriteLine("Enter Email");
             contact.Email = Console.ReadLine();
 
+            address.Add(contact);
             
         }
-        public void Display()
+        public void EditContact(string name)
         {
+            //AddContact();
             foreach (var contact in address)
             {
-                Console.WriteLine(contact.FirstName + "\n" + contact.LastName + "\n" + contact.Address + "\n" + contact.City + "\n" + contact.State + "\n" + contact.zip + "\n" + contact.PhoneNumber + "\n" + contact.Email);
+                if (contact.FirstName.Equals(name))
+                {
+                    Console.WriteLine("---------------------------\n");
+                    Console.WriteLine("Enter The Option To Update");
+                    Contact contact1 = new Contact();
+                    Console.WriteLine("1.FirstName \n 2.LastName \n 3.Address \n 4.City \n 5.State \n 6.zip \n 7.PhoneNumber \n 8.Email ");
+                    int option = Convert.ToInt32(Console.ReadLine());
+                    switch (option)
+                    {
+                        case 1:
+                            Console.WriteLine("Enter FirstName to Update");
+                            contact.FirstName = Console.ReadLine();
+                            break;
+                        case 2:
+                            Console.WriteLine("Enter LastName to Update");
+                            contact.LastName = Console.ReadLine();
+                            break;
+                        case 3:
+                            Console.WriteLine("Enter Address to Update");
+                            contact.Address = Console.ReadLine();
+                            break;
+                        case 4:
+                            Console.WriteLine("Enter City to Update");
+                            contact.City = Console.ReadLine();
+                            break;
+                        case 5:
+                            Console.WriteLine("Enter State to Update");
+                            contact.State = Console.ReadLine();
+                            break;
+                        case 6:
+                            Console.WriteLine("Enter Zip to Update");
+                            contact.zip = Convert.ToInt32(Console.ReadLine());
+                            break;
+                        case 7:
+                            Console.WriteLine("Enter PhoneNo to Update");
+                            contact.PhoneNumber = Convert.ToInt32(Console.ReadLine());
+                            break;
+                        case 8:
+                            Console.WriteLine("Enter Email to Update");
+                            contact.Email = Console.ReadLine();
+                            break;
+
+                            Console.WriteLine("....Edited Contact List........");
+                            Display();
+                    }
+                    
+                    
+                }
+                else
+                {
+                    Console.WriteLine("Unable to find name in address book");
+                }
             }
         }
+              
     }
 }
--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -5,10 +5,11 @@
         public static void Main(string[] args)
         {
             bool flag = true;
+            Address address = new Address();
             while (flag)
             {
-                Address address = new Address();
-                Console.WriteLine("\nselect Program\n 1.AddContacts");
+                
+                Console.WriteLine("\nselect Program\n 1.AddContacts \n 2.Display \n 3.EditContact"  );
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
@@ -17,6 +18,11 @@
                         break;
                     case 2:
                         address.Display();
+                        break;
+                    case 3:
+                        Console.WriteLine("Enter Name of Contact you want to edit");
+                        string name = Console.ReadLine();
+                        address.EditContact(name);
                         break;
                             
                     default:
